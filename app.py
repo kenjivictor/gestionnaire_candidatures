3bd89b51448ab7fb59a0a76401649fb3e558bb3d
kenjivictor <kenji.a.victor@gmail.com>
2026-04-07 11:56:44 +0200

    màj interface liste

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import tools.streamlit_widgets as st_widgets
 from pathlib import Path
 from dotenv import load_dotenv
 import uuid
+import base64
 
 # --- CONFIGURATION DES VARIABLES ---
 
@@ -96,10 +97,38 @@ def run_query(query, params=(), fetch=False):
 def color_result(val):
     if val == 'Refus': return 'background-color: #ffcccc' # Rouge clair
     if val == 'Entretien': return 'background-color: #ccffcc' # Vert clair
-    if val == 'Offre': return 'background-color: #ffff99; font-weight: bold' # Jaune/Or
+    if val == 'Offre': return 'background-color: #ffff99' # Jaune/Or
     return ''
 
 
+# fonction qui affiche un fichier PDF dans une fenetre modale
+@st.dialog("Visualisation du document", width="large")
+def display_pdf(file_path):
+    
+    # teste l'existance du fichier
+    if not os.path.exists(file_path):
+        st.error("Le fichier est introuvable sur le disque.")
+        return
+    
+    # Lecture du fichier et encodage en base64
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    
+    # Création de l'objet HTML (Iframe)
+    pdf_display = f'''
+            <div style="height: 80vh; width: 100%; overflow: hidden;">
+                <iframe 
+                    src="data:application/pdf;base64,{base64_pdf}#view=FitH" 
+                    width="100%" 
+                    height="100%" 
+                    type="application/pdf"
+                    style="border:none;"
+                ></iframe>
+            </div>
+                '''
+    
+    # Affichage dans Streamlit
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 # --- INTERFACE STREAMLIT ---
@@ -184,6 +213,7 @@ elif menu_selected == "Liste":
 
     if isinstance(raw_data, list):
         if len(raw_data) > 0:
+            
             df_stats = pd.DataFrame(raw_data, columns=['resultat'])
             total = len(df_stats)
             entretiens = len(df_stats[df_stats['resultat'] == 'Entretien'])
@@ -207,20 +237,22 @@ elif menu_selected == "Liste":
 
     
     st.divider()
+    
 
+    st.subheader("📈 Mes candidatures en cours")
 
     # affichage des candidatures
     data = run_query("SELECT * FROM candidatures WHERE resultat != 'Refus' ORDER BY date_candidature DESC", fetch=True)
     
     if isinstance(data, list):
-        st.subheader("⏳ Relances")
+        
         
         if len(data) >0:
 
             # Transformation des données en DataFrame pour manipulation facile
             df = pd.DataFrame(data)
 
-            # -- calcul des relances
+            # -- calcul de nouvelles colonnes
 
             # Conversion de la colonne date (qui est en texte/ISO dans SQLite) en objet datetime
             # errors='coerce' => Si la cellule est vide dans SQLite, Pandas créera un NaT (Not a Time)
@@ -229,54 +261,86 @@ elif menu_selected == "Liste":
 
             # Calcul du délai de relance (aujourd'hui - nb jours)
             seuil_relance = datetime.now() - timedelta(days=float(DELAI_RELANCE))
-
-            # Condition A : Candidatures 'En attente'
-            cond_attente = (df['resultat'] == 'En attente')
-
-            # Condition B : Si date_reponse est VIDE (NaT), on check date_candidature
-            cond_sans_rep = df['date_reponse'].isna() & (df['date_candidature'] < seuil_relance)
-
-            # Condition C : Si date_reponse est REMPLIE, on check date_reponse
-            cond_avec_rep = df['date_reponse'].notna() & (df['date_reponse'] < seuil_relance)
-
-            # Application du filtre final (Union des conditions B et C, limitée aux 'En attente')
-            a_relancer = df[cond_attente & (cond_sans_rep | cond_avec_rep)]
-
-            # On crée une colonne temporaire pour l'affichage du délai
-            df_alert = a_relancer.copy()
-            df_alert['jours_attente'] = (datetime.now() - df_alert['date_candidature']).dt.days
-
-
-            # affichage du message
-            if not df_alert.empty:
-                st.warning(f"⚠️ **Attention :** Tu as **{len(df_alert)}** candidature(s) sans réponse depuis plus de 15 jours. Il est temps de relancer !")
-                with st.expander("🔍 Voir les entreprises à relancer"):
-                    st.table(df_alert[['societe_nom', 'titre_poste', 'jours_attente', 'date_candidature', 'societe_mail']])
-            else:
-                st.success("✅ Toutes tes relances sont à jour !")
-        
-        else:
-            st.info("Aucune donnée disponible.")
             
-        st.divider()
+            # date de référence à prendre en compte selon s'il y a déjà eu un retour ou non
+            df['date_ref'] = df['date_reponse'].fillna(df['date_candidature'])
+            
+            # calcul des candidatures à relancer
+            df['a_relancer'] = (df['date_ref'] < seuil_relance).astype(int)
+
+            # Calcule du nombre de jours d'attente (si 'En attente' => nbjours, sinon 0)
+            # Calcul de la différence pour toutes les lignes, puis création de la nouvelle colonne
+            diff_jours = (datetime.now() - df['date_candidature']).dt.days
+            df['jours_attente'] = diff_jours.where(df['resultat'] == 'En attente', 0)
+            ids_alert = df[df['a_relancer'] == 1]['id'].to_list()
 
 
-        st.subheader("📈 Mes candidatures en cours")
-        
-        
-        if len(data) >0:
+            # -- affichage du message pour les relances
+            if df['a_relancer'].sum() >0:
+                st.warning(f"⚠️ **Attention :** Tu as **{df['a_relancer'].sum()}** candidature(s) sans réponse depuis plus de **{DELAI_RELANCE} jours**.")
+            
             # -- filtres
             search = st.text_input("Filtrer par société ou poste")
             if search:
                 df = df[df['societe_nom'].str.contains(search, case=False) | df['titre_poste'].str.contains(search, case=False)]
+            
 
-            # -- affichage des candidatures
-            #st.dataframe(df.drop(columns=['id']), width='content')
-            st.dataframe(
-                df.style.map(color_result, subset=['resultat']),
-                width='content',
-                hide_index=True
-                )
+            # -- affichage des candidatures en colonnes
+            nb_cols = 2
+            for i in range(0, len(df), nb_cols):
+                cols = st.columns(nb_cols) # On crée nb_cols colonnes pour cette ligne
+                
+                # On remplit chaque colonne du groupe actuel
+                for j in range(nb_cols):
+                    if i + j < len(df):
+                        with cols[j]:
+                            
+                            candidature = df.iloc[i+j]
+                            
+                            # On affiche la fiche dans une "Card" (st.container avec bordure)
+                            with st.container(border=True):
+                                
+                                if candidature['id'] in ids_alert:
+                                    text_color = ":yellow"
+                                    text_color_back = ":yellow-background"
+                                    text_icon = "⚠️"
+                                else :
+                                    text_color = ":blue"
+                                    text_color_back = ":blue-background"
+                                    text_icon = "ℹ️"
+                                
+                                is_alert = df[(df['id'] == candidature['id']) & (df['a_relancer'] ==1)]['jours_attente']
+                                if len(is_alert)>0:
+                                    text_jours = f"{text_color}[{text_color_back}[**" + str(is_alert.iloc[0]) + " jour(s)**]]"
+                                else:
+                                    text_jours = ""
+                                
+                                st.markdown(f"{text_color}[{text_color_back}[**{text_icon} {candidature['societe_nom']} - {candidature['titre_poste']} - {candidature['type_contrat']}**]]")
+                                st.caption(f"""
+                                        Candidature envoyée le **{datetime.strftime(candidature['date_candidature'], '%d/%m/%Y')}** en **'{candidature['type_candidature']}'**  
+                                        Canal d'envoi : {candidature['canal']}
+                                        """)
+                                st.write(f"""
+                                        **Etat :** {candidature['resultat']} depuis le {datetime.strftime(candidature['date_ref'], '%d/%m/%Y')} {text_jours}  
+                                        **Suite à donner :** {candidature['suite_a_donner']}
+                                        """)
+                                st.write(f"**Commentaires :** {candidature['commentaires']}")
+                                
+                                # Bouton pour voir le détail complet
+                                with st.expander("Détails"):
+                                    st.write(f"""
+                                            **Adresse :** {candidature['societe_adresse']}  
+                                            **Mail :** {candidature['societe_mail']}  
+                                            **Tel :** {candidature['societe_tel']}  
+                                            **Personne contact :** {candidature['societe_contact']}  
+                                            **Lien original :** {candidature['lien_offre']}
+                                            """)
+                                    
+                                    pdf_path = candidature['pdf_path']
+                                        # On vérifie que le fichier existe avant d'essayer de l'afficher
+                                    if pdf_path and os.path.exists(pdf_path):
+                                        if st.button("Voir le PDF", key=f"btn_pdf_{i+j}"):
+                                            display_pdf(pdf_path)
         
         else:
             st.info("Aucune donnée disponible.")
@@ -284,16 +348,6 @@ elif menu_selected == "Liste":
     elif data == -1:
         st.error("Une erreur est survenue lors de la requête SQL.")
 
-
-    # Petit outil pour ouvrir le PDF sauvegardé
-    with st.expander("📂 Consulter une offre sauvegardée"):
-        offres_pdf = [f for f in os.listdir(PDF_FOLDER_PATH) if f.endswith('.pdf')]
-        if offres_pdf:
-            file_to_open = st.selectbox("Choisir un fichier", offres_pdf)
-            with open(os.path.join(PDF_FOLDER_PATH, file_to_open), "rb") as f:
-                st.download_button("Télécharger/Voir le PDF", f, file_name=file_to_open)
-        else:
-            st.write("Aucun PDF stocké pour le moment.")
 
 elif menu_selected == "Modifier/Supprimer":
     
