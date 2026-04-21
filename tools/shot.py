88929900e5aea293619bfccb22ad30c7b714f9eb
kenjivictor <kenji.a.victor@gmail.com>
2026-04-21 12:15:39 +0200

    update capture pdf
    ajout gestion des cookies

--- tools/shot.py
+++ tools/shot.py
@@ -1,6 +1,6 @@
 import sys
 from playwright.sync_api import sync_playwright
-
+import time
 # Ce script ne fait qu'une chose : prendre une URL et en faire un PDF
 
 
@@ -8,10 +8,31 @@ def capture(url, output_path):
     try:
         with sync_playwright() as p:
             # Lancement du navigateur
-            browser = p.chromium.launch(headless=True)
+            # browser = p.chromium.launch(headless=True)
+            browser = p.chromium.launch()
             page = browser.new_page()
             # Navigation avec timeout
             page.goto(url, wait_until="networkidle", timeout=60000)
+            
+            # --- Gestion des cookies 
+            # On définit une liste de sélecteurs courants pour les boutons de cookies
+            cookie_selectors = [
+                "button:has-text('Continuer sans accepter')",
+                "button:has-text('Tout accepter')", 
+                "button:has-text('Accepter')",
+                "#didomi-notice-agree-button",
+                ".cookie-banner-accept"
+            ]
+
+            for selector in cookie_selectors:
+                try:
+                    # On vérifie si le bouton est visible et on clique
+                    if page.locator(selector).is_visible():
+                        page.locator(selector).click()
+                        page.wait_for_timeout(500) # Petit délai pour que la popup disparaisse
+                except:
+                    continue
+            
             # Generation du PDF
             page.pdf(path=output_path)
             browser.close()
